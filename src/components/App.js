fb6929f58edf765ae5f3cf63d9a6fdc70ba49610
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-27 19:12:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,24 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [bool, setBool] = useState(false);
+  function showData() {
+      setBool(true);
+  }
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+      <div id="main">
+          <button id="click" onClick={showData}>
+              on click
+          </button>
+          {bool ? (
+              <p id="para">
+                  Hello, I've learnt to use the full-stack evaluation tool.
+                  This makes me so happy
+              </p>
+          ) : (
+              ""
+          )}
+      </div>
   );
 }
 
